7c8daf1c10fe98ec113b18bb10afdaf799d300d9
Sridhar Thangavel <srigadgets@gmail.com>
2017-07-04 14:57:45 +0530

    Create LeastNumber.java

---
+++ LeastNumber.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class LeastNumber {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bb = new BufferedReader(new InputStreamReader(System.in));
+		Integer n,i,d,sub;
+		String ns,str="";		
+		ArrayList<Integer> ts = new ArrayList<Integer>();
+		
+		System.out.println("Enter the number : ");
+		n = Integer.parseInt(bb.readLine());
+		n = Math.abs(n);
+		
+		for(i=0;n>9;i++){
+				sub = n%10;
+				n /= 10;
+				ts.add(sub);
+		}
+		if(n>=0)
+			ts.add(n);
+		Collections.sort(ts);
+		
+		for(Integer ii : ts)
+			str += ii.toString();
+		System.out.println(str);
+		
+		System.out.println("Enter the number of digits want to delete : ");
+		d = Integer.parseInt(bb.readLine());
+		
+		System.out.println(str.substring(0,str.length()-d));
+				
+	}
+
+}
