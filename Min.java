39a19172276c7784b5bdd96c4de228fd52efa229
Sridhar Thangavel <srigadgets@gmail.com>
2017-07-04 14:05:35 +0530

    Add files via upload

---
+++ Min.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Min {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bb = new BufferedReader(new InputStreamReader(System.in));
+		Integer n,i,d,sub;
+		String ns,str="";		
+		TreeSet<Integer> ts = new TreeSet<Integer>();
+		
+		System.out.println("Enter the number : ");
+		n = Integer.parseInt(bb.readLine());
+		n = Math.abs(n);
+		
+		for(i=0;n>9;i++){
+				sub = n%10;
+				n /= 10;
+				if(sub>0)
+				ts.add(sub);
+		}
+		if(n>0)
+			ts.add(n);
+		
+		for(Integer ii : ts)
+			str += ii.toString();
+		//System.out.println(str);
+		
+		System.out.println("Enter the number of digits want to delete : ");
+		d = Integer.parseInt(bb.readLine());
+		
+		System.out.println(str.substring(0,str.length()-d));
+				
+	}
+
+}
