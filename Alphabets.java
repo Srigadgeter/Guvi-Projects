a6a2bdfeb4ca3962b826375f53dce12b35f06c65
Sridhar Thangavel <srigadgets@gmail.com>
2017-07-14 15:14:00 +0530

    Update Alphabets.java

--- Alphabets.java
+++ Alphabets.java
@@ -5,11 +5,11 @@ public class Alphabets {
 	public static void main(String[] args) throws IOException{
 		BufferedReader bb = new BufferedReader(new InputStreamReader(System.in));
 		char cc = (char) bb.read();
-		char i;
+		char i,j;
 		Integer flag = 0;
 		
-		for(i=65;i<=90;i++){
-			if(cc == i)
+		for(i=65,j=97;i<=90 && j<=122;i++,j++){
+			if(cc == i || cc == j)
 				flag = 1;
 		}
 		if(flag == 1)
